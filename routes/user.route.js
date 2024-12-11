b886ef2552a06f27ef5c09ee02fa6554fdb13e91
Himanshu Tiwari <htiwari578@gmail.com>
2024-12-11 21:23:53 +0530

    updated

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route('/register').post(register);
 router.route('/login').post(login);
-router.route('/logout').post(logout);
+router.route('/logout').get(logout);
 
 
 export default router;
