1a1d78c0ac18a2f81523216f6d53c53d6e588ac8
Himanshu Tiwari <htiwari578@gmail.com>
2024-12-13 15:55:20 +0530

    added socket-io

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import connectDB from "./utils/db.js";
-
+import cors from "cors";
+import { app,server } from "./socket/socket.js";
 import userRoute from './routes/user.route.js'
 import messageRoute from './routes/message.route.js'
 
@@ -12,10 +13,16 @@ import messageRoute from './routes/message.route.js'
 
 
 dotenv.config();
+app.use(express.urlencoded({extended:true}));
 const app = express();
 app.use(cookieParser());
 // midllware
 app.use(express.json());
+const corsOption = {
+    origin:'http://localhost:5173/',
+    credentials:true
+};
+app.use(cors(corsOption));
 
 const PORT = process.env.PORT || 3000;
 
